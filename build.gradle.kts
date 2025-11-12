3dd8c61ce3778ec697b46075278ed91b6cb3041c
Mounir Samite <mounir.samite@gmail.com>
2025-11-12 23:30:27 +0100

    fead: finish first build version for the projec

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,10 @@
 plugins {
-    kotlin("jvm") version "1.9.22"
+    kotlin("jvm") version "2.1.10"
     application
     jacoco
     id("org.jlleitschuh.gradle.ktlint") version "14.0.1"
+    id("io.gitlab.arturbosch.detekt") version "1.23.8"
+    id("org.jetbrains.dokka") version "2.1.0"
 }
 
 group = "com.heyteam.ufg"
@@ -12,12 +14,19 @@ repositories {
     mavenCentral()
 }
 
+detekt {
+    buildUponDefaultConfig = true
+    allRules = false
+    config.setFrom("$projectDir/config/detekt.yml")
+}
+
 dependencies {
     val kotestVersion = "5.8.0"
     implementation("org.jetbrains.kotlin:kotlin-stdlib")
     testImplementation("io.kotest:kotest-runner-junit5:$kotestVersion")
     testImplementation("io.kotest:kotest-assertions-core:$kotestVersion")
     testImplementation("io.kotest:kotest-property:$kotestVersion")
+    detektPlugins("io.gitlab.arturbosch.detekt:detekt-formatting:1.23.8")
 }
 
 application {
@@ -28,13 +37,10 @@ tasks.test {
     useJUnitPlatform()
 }
 
-tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
-    kotlinOptions {
-        allWarningsAsErrors = true
-    }
-}
-
 kotlin {
     val jdk = 17
     jvmToolchain(jdk)
+    compilerOptions {
+        allWarningsAsErrors.set(true)
+    }
 }
