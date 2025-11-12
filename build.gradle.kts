b9b2b9cffd0983403695674aa48cbd535ca12fbf
Mounir Samite <mounir.samite@gmail.com>
2025-11-12 22:52:03 +0100

    feat: gradle project setup finished

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,11 @@
 plugins {
     kotlin("jvm") version "1.9.22"
     application
+    jacoco
+    id("org.jlleitschuh.gradle.ktlint") version "14.0.1"
 }
 
-group = "com.heyteam.UFG"
+group = "com.heyteam.ufg"
 version = "1.0-SNAPSHOT"
 
 repositories {
@@ -11,8 +13,11 @@ repositories {
 }
 
 dependencies {
+    val kotestVersion = "5.8.0"
     implementation("org.jetbrains.kotlin:kotlin-stdlib")
-    testImplementation("org.jetbrains.kotlin:kotlin-test")
+    testImplementation("io.kotest:kotest-runner-junit5:$kotestVersion")
+    testImplementation("io.kotest:kotest-assertions-core:$kotestVersion")
+    testImplementation("io.kotest:kotest-property:$kotestVersion")
 }
 
 application {
@@ -23,10 +28,13 @@ tasks.test {
     useJUnitPlatform()
 }
 
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+    kotlinOptions {
+        allWarningsAsErrors = true
+    }
+}
+
 kotlin {
     val jdk = 17
     jvmToolchain(jdk)
-    compilerOptions {
-        allWarningsAsErrors.set(true)
-    }
 }
