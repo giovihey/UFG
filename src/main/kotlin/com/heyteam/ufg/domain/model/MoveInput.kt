bfe4da4a602c75b5eaa26865c884a69cf5d1f31c
Mounir Samite <mounir.samite@gmail.com>
2026-03-16 19:06:22 +0100

    chore: simplify movements

--- src/main/kotlin/com/heyteam/ufg/domain/model/MoveInput.kt
+++ src/main/kotlin/com/heyteam/ufg/domain/model/MoveInput.kt
@@ -1,8 +1,9 @@
 package com.heyteam.ufg.domain.model
 
 enum class MoveInput {
-    PUNCH_LIGHT,
-    PUNCH_HEAVY,
-    KICK_LIGHT,
-    KICK_HEAVY,
+    PUNCH,
+    KICK,
+    BLOCK,
+    JUMP,
+    MOVEMENT,
 }
