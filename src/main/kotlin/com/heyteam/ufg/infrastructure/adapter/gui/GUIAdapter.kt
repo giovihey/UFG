9bc109bb63aae089f4122459383e870e8988dfb5
Mounir Samite <mounir.samite@gmail.com>
2026-03-19 17:59:55 +0100

    feat: fuck you detekt

--- src/main/kotlin/com/heyteam/ufg/infrastructure/adapter/gui/GUIAdapter.kt
+++ src/main/kotlin/com/heyteam/ufg/infrastructure/adapter/gui/GUIAdapter.kt
@@ -1,5 +1,8 @@
 package com.heyteam.ufg.infrastructure.adapter.gui
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -8,6 +11,8 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.DpSize
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
@@ -43,13 +48,25 @@ class GUIAdapter : RenderPort {
 
     @Composable
     fun gameApp() {
+        background()
         counter()
     }
 
     @Composable
-    fun greetings() {
+    fun character() {
         val name = "mounir"
         Text("Hello, $name!")
+    }
+
+    @Composable
+    fun background() {
+        val color: Color = Color.Blue
+        Box(
+            modifier =
+                Modifier
+                    .fillMaxSize()
+                    .background(color),
+        )
     }
 
     @Composable
